f403ec485b380b454a3be5f81d6e37dc58500b25
Vitaliy Gonchar <weaxme@gmail.com>
2018-04-06 14:24:33 +0300

    First work version of tabbed-panel.js

--- orienteer-core/src/main/resources/org/orienteer/core/component/tabbed-panel.js
+++ orienteer-core/src/main/resources/org/orienteer/core/component/tabbed-panel.js
@@ -4,6 +4,7 @@
     function ODropdown(container) {
         this.container = container;
         this.dropdown = container.find('.last').first();
+        this.items = null;
         this.currentTab = this.createCurrentTabElement();
         this.menu = this.createDropdownMenu();
     }
@@ -28,16 +29,17 @@
         return div;
     };
 
-    ODropdown.prototype.createMenuItem = function (content) {
-        return content.find('a').first()
-            .addClass('dropdown-item');
+    ODropdown.prototype.prepareItem = function (item) {
+        return item.addClass('dropdown-item');
     };
 
-    ODropdown.prototype.render = function (items) {
+    ODropdown.prototype.render = function () {
         var self = this;
         var i, element;
-        for (i = 0; i < items.length; i++) {
-            element = self.createMenuItem(items[i]);
+        self.setVisible(true);
+        self.setDisplayText(null);
+        for (i = 0; i < self.items.length; i++) {
+            element = self.prepareItem(self.items[i]);
             if (element.hasClass('active')) {
                 self.setDisplayText(element.find('span').first().html());
             }
@@ -46,11 +48,31 @@
         self.dropdown.append(self.currentTab);
         self.dropdown.append(self.menu);
         self.container.append(self.dropdown);
+
     };
 
     ODropdown.prototype.setDisplayText = function (text) {
-        this.currentTab.find('span').first().replaceWith('<span>' + text + '</span>');
-        this.currentTab.addClass('active');
+        if (text) {
+            this.currentTab.find('span').first().replaceWith('<span>' + text + '</span>');
+            this.currentTab.addClass('active');
+        } else {
+            this.currentTab.find('span').first().replaceWith('<span></span>');
+            this.currentTab.removeClass('active');
+        }
+        return this;
+    };
+
+    ODropdown.prototype.setItems = function (items) {
+        this.items = items;
+        return this;
+    };
+
+    ODropdown.prototype.setVisible = function (visible) {
+        if (this.currentTab) {
+            if (visible) {
+                this.currentTab.show();
+            } else this.currentTab.hide();
+        }
         return this;
     };
 
@@ -67,73 +89,84 @@
             var self = this;
             self.screenWidth = width;
             var dropdownTabs = self.computeDropdownTabs(self.tabs);
-            var tabs = self.tabs.slice(0, self.tabs.length - dropdownTabs.length);
-            if (dropdownTabs.length <= 1) {
-                return;
-            }
-            // tabs.forEach(function (tab) {
-            //     if (!tab.hasClass('nav-item')) {
-            //         var li = $( document.createElement('li') );
-            //         li.addClass('nav-item');
-            //         tab.removeClass('dropdown-item').addClass('nav-link');
-            //         li.addClass(tab);
-            //         self.tabsContainer.append(li);
-            //     }
-            // });
-
-            self.prepareDropdownTabs(dropdownTabs);
-            self.dropdown.render(dropdownTabs);
+            var panelTabs = self.tabs.slice(0, self.tabs.length - dropdownTabs.length);
+            this.tabsContainer.find('li').each(function (i) {
+                if (i < panelTabs.length) {
+                    var el = panelTabs[i].element;
+                    el.removeClass('dropdown-item');
+                    el.addClass('nav-link');
+                    $(this).append(el);
+                }
+            });
+            if (dropdownTabs.length > 0) {
+                self.dropdown.setItems(self.prepareDropdownTabs(dropdownTabs));
+                self.dropdown.render();
+            } else self.dropdown.setVisible(false);
         }
     };
 
     OTabbedPanel.prototype.computeDropdownTabs = function (allTabs) {
         var max = this.searchMaxTabWidth(allTabs);
-        var availableWidth = this.screenWidth - max;
-        var firstWidth = allTabs[0].width();
-        var i;
+        var availableWidth = this.screenWidth - max - allTabs[0].width;
+        var i, computedWidth;
         for (i = 1; i < allTabs.length; i++) {
-            if (availableWidth <= firstWidth) {
+            computedWidth = availableWidth -= allTabs[i].width;
+            if (computedWidth <= 0) {
                 break;
             }
-            availableWidth -= allTabs[i].width();
+            availableWidth = computedWidth;
         }
-        return allTabs.slice(i - 1);
+        return i < allTabs.length ? allTabs.slice(i - 1) : [];
     };
 
     OTabbedPanel.prototype.prepareDropdownTabs = function (tabs) {
+        var elements = [];
         tabs.forEach(function(tab) {
-            tab.find('a').first().removeClass('nav-link');
+            var el = tab.element;
+            elements.push(
+                el.removeClass('nav-link')
+            );
         });
-        return tabs;
+        return elements;
     };
 
     OTabbedPanel.prototype.searchTabs = function (container) {
         var tabs = [];
         container.find('li').each(function () {
-            tabs.push( $(this) );
+            var tab = $(this);
+            tabs.push({
+                element: tab.find('a').first(),
+                width: tab.width()
+            });
         });
         return tabs;
     };
 
     OTabbedPanel.prototype.searchMaxTabWidth = function(tabs) {
-        var max = tabs[0].width();
-        var width;
+        var max = tabs[0].width;
         tabs.forEach(function (t) {
-            width = t.width();
-            if (width > max) {
-                max = width;
+            if (t.width > max) {
+                max = t.width;
             }
         });
         return max;
     };
-
 
     $.fn.tabbedPanel = function (options) {
         return this.each(function () {
             var panel = new OTabbedPanel( $(this) );
             panel.render();
             var callback = $.proxy(panel.render, panel);
-            $(window).resize(callback);
+            var lock = false;
+            $(window).resize(function() {
+                if (!lock) {
+                    lock = true;
+                    setTimeout(function() {
+                        callback();
+                        lock = false;
+                    }, 10);
+                }
+            });
         });
     };
 })(jQuery);
